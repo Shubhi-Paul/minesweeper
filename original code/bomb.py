ac37288b9510191adc6be453a42279d1101f29a8
Shubhi <shubhi.paul2021@vitstudent.ac.in>
2021-10-08 14:12:28 +0530

    commit

--- original code/bomb.py
+++ original code/bomb.py
@@ -7,28 +7,29 @@ def minesweeper(n):
     if (x >= 1 and x <= 3):
         arr[y][x+1] += 1 # center right
         arr[y][x-1] += 1 # center left
-    if (x == 0):
+    elif (x == 0):
         arr[y][x+1] += 1 # center right
-    if (x == 4):
+    elif (x == 4):
         arr[y][x-1] += 1 # center left
+
     if (x >= 1 and x <= 4) and (y >= 1 and y <= 4):
         arr[y-1][x-1] += 1 # top left
- 
-    if (x >= 0 and x <= 3) and (y >= 1 and y <= 4):
+    elif (x >= 0 and x <= 3) and (y >= 1 and y <= 4):
         arr[y-1][x+1] += 1 # top right
-    if (x >= 0 and x <= 4) and (y >= 1 and y <= 4):
+    elif (x >= 0 and x <= 4) and (y >= 1 and y <= 4):
         arr[y-1][x] += 1 # top center
  
     if (x >=0 and x <= 3) and (y >= 0 and y <= 3):
         arr[y+1][x+1] += 1 # bottom right
-    
-    if (x >= 1 and x <= 4) and (y >= 0 and y <= 3):
+    elif (x >= 1 and x <= 4) and (y >= 0 and y <= 3):
         arr[y+1][x-1] += 1 # bottom left
-    if (x >= 0 and x <= 4) and (y >= 0 and y <= 3):
+    elif (x >= 0 and x <= 4) and (y >= 0 and y <= 3):
         arr[y+1][x] += 1 # bottom center
+    
     for row in arr:
         print(" ".join(str(cell) for cell in row))
         print("")
+
 if __name__ == "__main__":
     minesweeper(5)
 
